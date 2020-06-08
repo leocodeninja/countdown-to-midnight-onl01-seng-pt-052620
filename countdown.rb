ec310989a29225d544b50427d11ee4f18518e220
Leonardo Newton <leo253@protonmail.com>
2020-06-08 23:20:19 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
+def countdown(number)
+until number == 0
+  number.times do
+    puts"#{number} SECOND(S)!"
+    number -= 1
+  end
+end
+ return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(number)
+until number == 0
+  number.times do
+    puts"#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+end
+ return "HAPPY NEW YEAR!"
 end
